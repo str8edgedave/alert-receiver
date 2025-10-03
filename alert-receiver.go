26ea744c1853deb237b8e29f1bab86e0301b19b6
David Yaffe <david.yaffe@sasktel.com>
2025-10-03 10:10:00 -0600

    added client connection information

--- alert-receiver.go
+++ alert-receiver.go
@@ -6,12 +6,14 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 // Set up REST API health check endpoint
 func handleHealth(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "200 OK\n")
+	w.Write([]byte("Request received\n"))
+	log.Println("200 OK\n")
 }
 
 // Set up alert receiver endpoint
@@ -21,15 +23,52 @@ func handleAlertReceiver(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Timestamp in ISO 8601 format
+	timestamp := time.Now().Format(time.RFC3339)
+
+	// Print request method and URL
+	fmt.Printf("Received %s request for %s\n", r.Method, r.URL.Path)
+
+	// Print Connection information
+	fmt.Println("Connection Information")
+	fmt.Printf("  Time: %s\n", timestamp)
+	fmt.Printf("  Client IP/Port: %s\n", r.RemoteAddr)
+	fmt.Printf("  Protocol: %s\n", r.Proto)
+	fmt.Printf("  URI: %s\n", r.RequestURI)
+
+	// Print headers
+	fmt.Println("Headers:")
+	for name, values := range r.Header {
+		for _, value := range values {
+			fmt.Printf("  %s: %s\n", name, value)
+		}
+	}
+
 	// Read request body
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		log.Printf("Error reading request body: %v", err)
 		http.Error(w, "Failed to read request body", http.StatusBadRequest)
 		return
+	} else {
+		fmt.Println("Body:")
+		fmt.Println(string(body))
 	}
+
+	//dump, err := httputil.DumpRequest(r, true)
+	//if err != nil {
+	//	http.Error(w, "Failed to dump request: "+err.Error(), http.StatusInternalServerError)
+	//	return
+	//}
+
+	//fmt.Println("Full HTTP Request:")
+	//fmt.Println(string(dump))
+
+	// Respond to the client
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Request received\n"))
+
 	defer r.Body.Close()
-	fmt.Println(body)
 }
 
 // Check for ENV and return
@@ -41,21 +80,16 @@ func getEnv(key, defaultValue string) string {
 }
 
 func main() {
-        defer func() {
-	log.Println("Exiting application.")
+	defer func() {
+		log.Println("Exiting application.")
 	}()
 
 	http.HandleFunc("/health", handleHealth)
 	http.HandleFunc("/receiver", handleAlertReceiver)
 
-	//
 	port := getEnv("PORT", "8080")
 	host := getEnv("HOST", "127.0.0.1")
-	if host == "127.0.0.1" || host == "localhost" {
-		log.Printf("Starting server on port %s", port)
-	} else {
-		log.Printf("Starting server on %s port %s", host, port)
-	}
+	log.Printf("Starting server on %s port %s", host, port)
 	log.Printf("Health endpoint: http://%s:%s/health", host, port)
 	log.Printf("Alert Receiver endpoint: http://%s:%s/receiver", host, port)
 
